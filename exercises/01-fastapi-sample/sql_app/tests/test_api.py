fab531b778bb5b54c1b58b578d9339f3dee1f452
nakasei <e175711@ie.u-ryukyu.ac.jp>
2023-04-15 16:01:26 +0900

    fixing and adding of test

--- exercises/01-fastapi-sample/sql_app/tests/test_api.py
+++ exercises/01-fastapi-sample/sql_app/tests/test_api.py
@@ -5,12 +5,27 @@ def test_create_user(test_db, client):
     )
     assert response.status_code == 200, response.text
     data = response.json()
-    assert data["email"] == "deadpool@example.com"
-    assert "id" in data
-    user_id = data["id"]
+    db_user = data["db_user"]
+    assert db_user["email"] == "deadpool@example.com"
+    assert "id" in db_user
+    user_id = db_user["id"]
 
-    response = client.get(f"/users/{user_id}")
+    token = data["x_api_token"]
+    headers = {"Authorization": f"Bearer {token}"}
+    
+    # 正常系
+    response = client.get(f"/users/{user_id}", headers=headers)
     assert response.status_code == 200, response.text
     data = response.json()
     assert data["email"] == "deadpool@example.com"
     assert data["id"] == user_id
+
+    # 異常系（トークンあり）
+    headers = {"Authorization": f"Bearer fake"}
+    response = client.get(f"/users/{user_id}", headers=headers)
+    assert response.status_code == 401, response.text
+
+    # 異常系（トークンなし）
+    headers = {"Authorization": f""}
+    response = client.get(f"/users/{user_id}", headers=headers)
+    assert response.status_code == 403, response.text
