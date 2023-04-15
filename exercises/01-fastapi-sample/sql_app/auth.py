ffaf9863852d3dc4e4c5269fbee7c468743e92c4
nakasei <e175711@ie.u-ryukyu.ac.jp>
2023-04-15 14:58:11 +0900

    applying certification

--- exercises/01-fastapi-sample/sql_app/auth.py
+++ exercises/01-fastapi-sample/sql_app/auth.py
@@ -1,5 +1,10 @@
 from jose import jwt
+from jose.exceptions import JWTError, ExpiredSignatureError
 from datetime import datetime, timedelta
+from sqlalchemy.orm import Session
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
 
 # どのアルゴリズムを使用して電子署名を行うか
 # 使用できるアルゴリズムはこちら(https://python-jose.readthedocs.io/en/latest/jws/index.html)
@@ -8,11 +13,12 @@ ALGORITHM = "HS256"
 # 暗号化に使用する鍵情報
 SECRET_KEY = "SECRET_KEY123"
 
+security = HTTPBearer()
+
 def create_tokens(user_id: int):
     """パスワード認証を行い、トークンを生成"""
     # ペイロード作成
     access_payload = {
-        'token_type': 'x_api_token',
         'exp': datetime.utcnow() + timedelta(minutes=60),
         'user_id': user_id,
     }
@@ -20,4 +26,36 @@ def create_tokens(user_id: int):
     # トークン作成（本来は'SECRET_KEY123'はもっと複雑にする）
     x_api_token = jwt.encode(access_payload, SECRET_KEY, algorithm=ALGORITHM)
 
-    return {'x_api_token': x_api_token, 'token_type': 'bearer'}
+    return x_api_token
+
+def verify_jwt(token: str):
+    try:
+        """tokenからユーザーを取得"""
+        # トークンをデコードしてペイロードを取得。有効期限と署名は自動で検証される。
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+    except ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token has expired",
+        )
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token signature",
+        )
+    return payload
+
+async def get_current_user(token: HTTPAuthorizationCredentials = Depends(security)):
+    if not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Not authenticated',
+        )
+    try:
+        payload = verify_jwt(token.credentials)
+    except HTTPException as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Failed",
+        )
+    return payload.get('user_id')
--- exercises/01-fastapi-sample/sql_app/main.py
+++ exercises/01-fastapi-sample/sql_app/main.py
@@ -34,17 +34,18 @@ def create_user(user: schemas.UserCreate, db: Session = db_session):
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     db_user = crud.create_user(db=db, user=user)
-    return {"db_user": db_user, "token_info": auth.create_tokens(db_user.id)}
+    return {"db_user": db_user, "x_api_token": auth.create_tokens(db_user.id)}
 
 
 @app.get("/users/", response_model=List[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = db_session):
+def read_users(skip: int = 0, limit: int = 100, db: Session = db_session, current_user: str = Depends(auth.get_current_user)):
+    print("aaaaaaaaaaaaaaaaaaaaaa")
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
 
 @app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = db_session):
+def read_user(user_id: int, db: Session = db_session, current_user: str = Depends(auth.get_current_user)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
